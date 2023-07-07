f5ec9a54e5bcae34ef1d479aac3933a8cae23986
lgh <949582554@qq.com>
2023-07-07 10:44:03 +0800

    添加express-session;添加获取验证码接口

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,22 @@
 import { NestFactory } from '@nestjs/core';
 import { VersioningType } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 接口版本控制
   app.enableVersioning({ type: VersioningType.URI });
+
+  // 添加session
+  app.use(
+    session({
+      secret: 'xiaoguang', //生成服务端session签名，可以理解为加盐
+      name: 'nest-demo.sid', //生成客户端cookie的名字，默认 connect.sid
+      rolling: true, // 在每次请求时强行设置cookie，这将重置cookie过期时间（默认为false）
+      cookie: { maxAge: null }, //设置返回前端浏览器cookie的配置，配置默认值：{ path: '/', httpOnly: true, secure: false, maxAge: null }
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,10 +7,13 @@ import {
   Param,
   Delete,
   Version,
+  Req,
+  Res,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as svgCaptcha from 'svg-captcha';
 
 @Controller({
   path: 'user',
@@ -44,5 +47,40 @@ export class UserController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
+  }
+
+  /**
+   * 生成随机验证码
+   */
+  @Get('code')
+  createCaptcha(@Req() req, @Res() res) {
+    console.log(req.session);
+    const captcha = svgCaptcha.create({
+      size: 4, //生成字符的个数
+      fontSize: 50, //文字大小
+      width: 100, //图片宽度
+      height: 34, // 图片高度
+      background: '#fff', //图片背景颜色
+    });
+    req.session.code = captcha.text; // 存储验证码记录到session
+    res.type('image/svg+xml');
+    res.send(captcha.data);
+  }
+
+  @Post('login')
+  login(@Req() req, @Body() body) {
+    console.log(req.session);
+    console.log('用户信息', body);
+    if (req.session?.code?.toLowerCase() === body?.code?.toLowerCase()) {
+      return {
+        success: true,
+        message: '登录成功',
+      };
+    } else {
+      return {
+        success: false,
+        message: '验证码错误，登录失败',
+      };
+    }
   }
 }
